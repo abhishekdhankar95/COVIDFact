7e39c36471be5d8c3d90e5625556d14ad84cd360
Hamman Samuel <4349051+hwsamuel@users.noreply.github.com>
2020-09-18 12:57:49 -0400

    Update med_non_med.py
    Removing commented lines

--- med_non_med.py
+++ med_non_med.py
@@ -22,14 +22,12 @@ from sklearn.naive_bayes import GaussianNB
 
 def conversion(training_data_csv, vector_csv):
 	training_data_csv.dropna(inplace=True)
-	# brief_cleaning = (sub("[^A-Za-z]+", ' ', str(row)).lower() for row in df_temp_clean['temp_clean'])
 	
 	for index, datapoint in training_data_csv.iterrows():
 		temp_list = []
 		for sentence in split_sentences(datapoint.text):
 			sentence = re.sub("[^A-Za-z]+", ' ', str(sentence)).lower()
 			sentence = re.sub(r'\s+', ' ', str(sentence))
-			#print(word_tokenize(sentence))
 			temp_list.append(encode(word_tokenize(sentence)))
 		temp_df = np.array(temp_list)
 		temp_df = np.average(temp_df, axis=0)
@@ -37,18 +35,9 @@ def conversion(training_data_csv, vector_csv):
 		temp_df.to_csv(vector_csv, index=False, header=False, mode='a')
 
 
-# google only modification 
-# google only csvs ok
-
-#vector_csv = 'training_data_vector_new_combined_10.csv'
-
-#og_encoder = api.load("word2vec-google-news-300")
 og_encoder = KeyedVectors.load_word2vec_format('word2vec_twitter_tokens_getit_actual.bin', binary=True, unicode_errors='ignore')
-#encoder = load(open('w2v_model_google_true.p', 'rb'))
 encoder = load(open('w2v_model_pubmed_opinion_sk_5_5.p', 'rb'))
-#nn_model = load(open('trained_regressor_google_combined.p', 'rb'))
 nn_model = load(open('trained_regressor_model_pubmed_opinion_sk_5_5.p', 'rb'))
-
 
 data_filename = 'tweets_health_curated_list_mod.csv'
 training_data_filename = 'training_data_sick.csv'
@@ -57,11 +46,8 @@ testing_data_filename = 'testing_data_sick.csv'
 vector_data_final = 'vector_data_og.csv'
 vector_testdata_final = 'testset_final_sick.csv'
 
-
-# remove this when done
 df = DataFrame(columns=['vector', 'label'])
 df.to_csv(vector_data_final, index=False)
-
 
 def encode(tokens, label=None):
 	X = []
@@ -70,31 +56,15 @@ def encode(tokens, label=None):
 		if token in og_encoder.vocab:
 			X.append(og_encoder[token])
 			y.append(label)
-		
-		#if token in og_encoder.vocab:
-		#	X.append(nn_model.predict(og_encoder[token].reshape(1, -1))[0])
-		#	y.append(label)
-		
-		#elif token in encoder.wv.vocab:
-		#	X.append(encoder.wv.get_vector(token))
-		#	y.append(label)
-		
-		#else:
-		#	#print(encoder.vector_size)
-		#	X.append(np.zeros(encoder.vector_size))
-		#	y.append(-1)
 	if not X:
 		df = np.zeros(encoder.vector_size)
 		return df
 	else:
 		df = np.array(X)
 	return np.average(df, axis=0)
-	# return (X,y)
-
 
 def tokenize(sentence):
 	tokens = word_tokenize(sentence)
-	# tokens = [t1.lower() for t1 in tokens if t1 not in punctuation] # Remove punctuations & stop words using Glasgow stop words list
 	return tokens
 	
 def convert_str_dataframe(df_vector, X, y):
@@ -108,26 +78,18 @@ def convert_str_dataframe(df_vector, X, y):
 	
 
 if __name__=='__main__':
-	
 	training_data_csv = read_csv(data_filename, usecols=['id', 'text', 'label'], dtype={'id':str, 'desc':str, 'label':str})	
-	#testing_data_csv = read_csv(testing_data_filename)
-	#training_data_csv.label = 1-training_data_csv.label
 	
 	conversion(training_data_csv, vector_data_final)
-	#conversion(testing_data_csv, vector_testdata_final)
 	
-
-	#lst_string = training_data_vector_shuffle[~training_data_vector_shuffle.vector.str.contains('nan')]
 	X_train = []
 	y_train = []
 	X_test = []
 	y_test = []
 	
 	training_data_vector_training_shuffle = read_csv(vector_data_final)
-	#training_data_vector_testing_shuffle = read_csv(vector_testdata_final)
 	
 	convert_str_dataframe(training_data_vector_training_shuffle, X_train, y_train)
-	#convert_str_dataframe(training_data_vector_testing_shuffle, X_test, y_test)
 	'''
 	parameters = {'kernel':['rbf'], 'C':[10]}
 	svc = svm.SVC(gamma='scale', probability=True, random_state=2)
@@ -165,15 +127,3 @@ if __name__=='__main__':
 	print(clf.loss_)
 	print(clf.score(X_test, y_test))
 	'''
-	
-	
-	#print(y_test)
-	#print(precision_recall_fscore_support(y_test, clf.predict(X_test), labels=[0, 1]))
-	# df = np.array(encode(['virus', 'coronavirus', 'covid-19'])[0])
-	# print(np.average(df, axis=0))
-	
-
-
-
-
-
